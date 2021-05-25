bc58ecad5aed3cdc918888de77acb366df1d2139
luisbeleno <luis.beleno@viamericas.com>
2021-05-25 17:39:31 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  {
+    path: 'usuario', loadChildren: () => import('./components/usuario/usuario.module')
+      .then(m => m.UsuarioModule)
+  },
+  {
+    path: 'dashboard', component: DashboardComponent,
+      loadChildren: () => import('./components/dashboard/dashboard.module')
+      .then(m => m.DashboardModule)
+  },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDC5A0K-PSCMTYaj2Yb8w92UhZGqoWSU8o',
+    authDomain: 'trivia-bible-app.firebaseapp.com',
+    projectId: 'trivia-bible-app',
+    storageBucket: 'trivia-bible-app.appspot.com',
+    messagingSenderId: '1076799442095',
+    appId: '1:1076799442095:web:4af72aae620459f001585a'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
